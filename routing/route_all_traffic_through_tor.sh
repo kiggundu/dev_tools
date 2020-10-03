009b16e3f203ac2340fb3757dad21b71d0ffd498
Abraham Kiggundu <abraham@thekiggys.com>
2020-10-03 22:56:39 +0100

    Working tor router... need to use mitmproxy with proxychains now

--- routing/route_all_traffic_through_tor.sh
+++ routing/route_all_traffic_through_tor.sh
@@ -15,7 +15,7 @@ _trans_port="9050"
 _dns_port="53"
 
 # Tor's VirtualAddrNetworkIPv4
-_virt_addr="192.168.0.1/24"
+_virt_addr="10.192.0.0/10"
 
 # Your outgoing interface
 _out_if="wlan0"
@@ -30,18 +30,18 @@ _non_tor="127.0.0.0/8 10.0.0.0/8 172.16.0.0/12 192.168.0.0/16"
 # Other IANA reserved blocks (These are not processed by tor and dropped by default)
 _resv_iana="0.0.0.0/8 100.64.0.0/10 169.254.0.0/16 192.0.0.0/24 192.0.2.0/24 192.88.99.0/24 198.18.0.0/15 198.51.100.0/24 203.0.113.0/24 224.0.0.0/4 240.0.0.0/4 255.255.255.255/32"
 
-### ensure all ssh trafic is not tored
-iptables -t nat -A PREROUTING -i $_inc_if -p tcp --dport 22 -j ACCEPT
-iptables -t nat -A PREROUTING -i $_out_if -p tcp --dport 22 -j ACCEPT
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+
+##########################################################################################
+# SSH traffic rules
+###########################################################################################
+### ensure all ssh trafic for all interfaces is not redirected to Tor
+sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j RETURN
 
 
-### *nat PREROUTING (For middlebox)
-sudo iptables -t nat -A PREROUTING -d $_virt_addr -i $_inc_if -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $_trans_port
-#sudo iptables -t nat -A PREROUTING -i $_inc_if -p udp --dport 53 -j REDIRECT --to-ports $_dns_port
-
-# Allow lan access for hosts in $_non_tor
+##########################################################################################
+# Rules governing LAN traffic
+###########################################################################################
+### Ensure all lan foffic from the incomming interface is not redirected to Tor
 for _lan in $_non_tor; do
     sudo iptables -t nat -A PREROUTING -i $_inc_if -d $_lan -j RETURN
 done
@@ -50,19 +50,29 @@ for _iana in $_resv_iana; do
     sudo iptables -t nat -A PREROUTING -i $_inc_if -d $_iana -j RETURN
 done
 
-sudo iptables -t nat -A PREROUTING -i $_inc_if -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $_trans_port
 
+##########################################################################################
+# Rules redirecting to Tor (TCP) and Stubby (DNS)
+###########################################################################################
+### Redirect all remaining incoming TCP:* and UDP:53 packets to the Tor port.
+sudo iptables -t nat -A PREROUTING -i $_inc_if -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $_trans_port
+sudo iptables -t nat -A PREROUTING -i $_inc_if -p udp -m udp --dport 53 -j REDIRECT --to-ports $_dns_port
 ### *nat OUTPUT (For local redirection)
-# nat .onion addresses
+# Ensure all .onion address destined communication from the router go tothe tor port
 sudo iptables -t nat -A OUTPUT -d $_virt_addr -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $_trans_port
 
-# nat dns requests to Stubby but do not nat requests originating from stubby
-#sudo iptables -t nat -A OUTPUT -p udp -m udp --dport 53 -j REDIRECT --to-ports $_dns_port
-#sudo iptables -t nat -A OUTPUT -m owner --uid-owner $_stubby_uid -j RETURN
 
-# Don't nat the Tor process, the loopback, or the local network
+##########################################################################################
+# Rules for locally originated traffic
+###########################################################################################
+# nat all local dns requests to Stubby except those requests originating from stubby
+sudo iptables -t nat -A OUTPUT -m owner --uid-owner $_stubby_uid -j RETURN
+#TODO: Disabled local DNS redirection for now. All subsequent commands fail with 'unable to resolve host ubuntu: Temporary failure in name resolution'
+#sudo iptables -t nat -A OUTPUT -p udp -m udp --dport 53 -j REDIRECT --to-ports $_dns_port
+# Don't nat the Tor process, the loopback, or the local network, o ssh port 22
 sudo iptables -t nat -A OUTPUT -m owner --uid-owner $_tor_uid -j RETURN
 sudo iptables -t nat -A OUTPUT -o lo -j RETURN
+sudo iptables -t nat -A OUTPUT -p tcp --dport 22 -j RETURN
 
 # Allow lan access for hosts in $_non_tor
 for _lan in $_non_tor; do
@@ -73,13 +83,16 @@ for _iana in $_resv_iana; do
     sudo iptables -t nat -A OUTPUT -d $_iana -j RETURN
 done
 
-# Redirect all other pre-routing and output to Tor's TransPort
+# Redirect all other locally originated tcp trafic to Tor's TransPort
 sudo iptables -t nat -A OUTPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j REDIRECT --to-ports $_trans_port
 
+
+##########################################################################################
+# Rules for trafic specifically destined for this router
+###########################################################################################
 ### *filter INPUT
 # Don't forget to grant yourself ssh access from remote machines before the DROP.
-sudo iptables -A INPUT -i $_out_if -p tcp --dport 22 -m state --state NEW -j ACCEPT
-
+sudo iptables -A INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT
 sudo iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT
 sudo iptables -A INPUT -i lo -j ACCEPT
 
@@ -89,13 +102,15 @@ sudo iptables -A INPUT -d $_inc_ip -i $_inc_if -p tcp -m tcp --dport $_trans_por
 
 # Allow INPUT from lan hosts in $_non_tor
 # Uncomment these 3 lines to enable.
-#for _lan in $_non_tor; do
-# sudo iptables -A INPUT -s $_lan -j ACCEPT
-#done
+for _lan in $_non_tor; do
+ sudo iptables -A INPUT -s $_lan -j ACCEPT
+done
 
 # Log & Drop everything else. Uncomment to enable logging.
 sudo iptables -A INPUT -j LOG --log-prefix "Dropped INPUT packet: " --log-level 7 --log-uid
 sudo iptables -A INPUT -j DROP
+
+
 
 ### *filter FORWARD
 sudo iptables -A FORWARD -j DROP
@@ -106,7 +121,7 @@ sudo iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
 
 # Allow Tor and Stubby  process output
 sudo iptables -A OUTPUT -o $_out_if -m owner --uid-owner $_tor_uid -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j ACCEPT
-sudo iptables -A OUTPUT -o $_out_if -m owner --uid-owner $_stubby_uid -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j ACCEPT
+sudo iptables -A OUTPUT -o $_out_if -m owner --uid-owner $_stubby_uid -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
 
 # Allow loopback output
 sudo iptables -A OUTPUT -d 127.0.0.1/32 -o lo -j ACCEPT
@@ -116,8 +131,8 @@ sudo iptables -A OUTPUT -d 127.0.0.1/32 -p tcp -m tcp --dport $_trans_port --tcp
 
 # Allow OUTPUT to lan hosts in $_non_tor
 # Uncomment these 3 lines to enable.
-#for _lan in $_non_tor; do
-# sudo iptables -A OUTPUT -d $_lan -j ACCEPT
+for _lan in $_non_tor; do
+ sudo iptables -A OUTPUT -d $_lan -j ACCEPT
 #done
 
 # Log & Drop everything else. Uncomment to enable logging
