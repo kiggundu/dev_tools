62b211d5d7684696b645ace01fa5ce48426b5e89
Abraham Kiggundu <abraham@thekiggys.com>
2023-03-20 17:52:58 +0000

    new script to install everything

---
+++ install_the_lot.sh
@@ -0,0 +1,14 @@
+./install_apt_tools.sh
+./install-tor.sh
+./install_jenv.sh
+./install_pyenv.sh
+./install_sdkman.sh
+./install_fzf_linux.sh
+./install_ohmyzsh.sh
+./install-truecrypt.sh
+./install_vimruntime.sh
+./install_qutebrowser.sh
+./install_gnuradio.sh
+./install_variety_desktop.sh
+./install_my_dotfiles.zsh
+
